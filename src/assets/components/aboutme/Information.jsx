f1c3d53f1819b8e3d5f6be3fc9c276606c97e32e
Matthew Houston <matthouston027@gmail.com>
2023-05-01 07:29:38 +0900

    Image files not uploading properly - take 5

--- src/assets/components/aboutme/Information.jsx
+++ src/assets/components/aboutme/Information.jsx
@@ -1,4 +1,5 @@
-import styles from "./Information.module.css"
+import styles from "./Information.module.css";
+import profileImg from "../../img/profile.png";
 
 const Information = () => {
 
@@ -6,7 +7,7 @@ const Information = () => {
         <>
             <section className={styles.information}>
                 <h1 className={styles.title}>About Me</h1>
-                <img src="src/assets/img/profile.png" alt="Hi, I'm Matthew!" className={styles.info_img} />
+                <img src={profileImg} alt="Hi, I'm Matthew!" className={styles.info_img} />
                 <div className={styles.info_text}>
                     <p>
                         I'm a self-taught developer in the Dallas-Fort Worth Metroplex who values communication, perseverence, and life long-learning.
--- src/assets/components/portfolio/Portfolio.jsx
+++ src/assets/components/portfolio/Portfolio.jsx
@@ -1,5 +1,10 @@
 import styles from "./Portfolio.module.css";
 import Shapes from "../shapes/Shapes"
+import yeti from '../../img/Yeti.png';
+import shark from '../../img/image 6.png';
+import weight from '../../img/weight.png';
+import tudor from '../../img/Tudo-r.png';
+import meteor from '../../img/image 7.png';
 
 const Portfolio = (props) => {
     const { circleType, triangleType } = props;
@@ -9,19 +14,19 @@ const Portfolio = (props) => {
             <section className={styles.portfolio}>
                 <div className={styles.carousel}>
                     <a href="https://yetijapan.com/" target="_blank">
-                        <img src="/src/assets/img/Yeti.png" alt="Yeti Japan" />
+                        <img src={yeti} alt="Yeti Japan" />
                     </a>
                     <a href="https://chimerical-puppy-f8d0d5.netlify.app/" target="_blank">
-                        <img src="/src/assets/img/image 6.png" alt="Pool Shark" />
+                        <img src={shark} alt="Pool Shark" />
                     </a>
                     <a href="https://adorable-twilight-fb881c.netlify.app/" target="_blank">
-                        <img src="./src/assets/img/weight.png" alt="" />
+                        <img src={weight} alt="" />
                     </a>
                     <a href="https://lustrous-bublanina-b8b4b7.netlify.app/" target="_blank">
-                        <img src="/src/assets/img/Tudo-r.png" alt="" />
+                        <img src={tudor} alt="" />
                     </a>
                     <a href="https://frabjous-biscuit-20fc2a.netlify.app/" target="_blank">
-                        <img src="/src/assets/img/image 7.png" alt="" />
+                        <img src={meteor} alt="" />
                     </a>
 
                 </div>
