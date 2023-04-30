f5968457486a8f5208e668bd6d7820ea0e036df5
Matthew Houston <matthouston027@gmail.com>
2023-05-01 02:52:53 +0900

    Image files not uploading properly - take 3

--- src/assets/components/portfolio/Portfolio.jsx
+++ src/assets/components/portfolio/Portfolio.jsx
@@ -9,19 +9,19 @@ const Portfolio = (props) => {
             <section className={styles.portfolio}>
                 <div className={styles.carousel}>
                     <a href="https://yetijapan.com/" target="_blank">
-                        <img src="src/assets/img/Yeti.png" alt="Yeti Japan" />
+                        <img src="/assets/img/Yeti.png" alt="Yeti Japan" />
                     </a>
                     <a href="https://chimerical-puppy-f8d0d5.netlify.app/" target="_blank">
-                        <img src="src/assets/img/image 6.png" alt="Pool Shark" />
+                        <img src="./assets/img/image 6.png" alt="Pool Shark" />
                     </a>
                     <a href="https://adorable-twilight-fb881c.netlify.app/" target="_blank">
-                        <img src="src/assets/img/weight.png" alt="" />
+                        <img src="assets/img/weight.png" alt="" />
                     </a>
                     <a href="https://lustrous-bublanina-b8b4b7.netlify.app/" target="_blank">
-                        <img src="src/assets/img/Tudo-r.png" alt="" />
+                        <img src="./assets/img/Tudo-r.png" alt="" />
                     </a>
                     <a href="https://frabjous-biscuit-20fc2a.netlify.app/" target="_blank">
-                        <img src="src/assets/img/image 7.png" alt="" />
+                        <img src="./assets/img/image 7.png" alt="" />
                     </a>
 
                 </div>
