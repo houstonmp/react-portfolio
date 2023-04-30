d3245f117c297a6df3546c86b6a3176e10fc3fca
Matthew Houston <matthouston027@gmail.com>
2023-05-01 02:43:57 +0900

    Image files not uploading properly - take two

--- src/assets/components/aboutme/Information.jsx
+++ src/assets/components/aboutme/Information.jsx
@@ -6,7 +6,7 @@ const Information = () => {
         <>
             <section className={styles.information}>
                 <h1 className={styles.title}>About Me</h1>
-                <img src="src\assets\img\profile.png" alt="Hi, I'm Matthew!" className={styles.info_img} />
+                <img src="src/assets/img/profile.png" alt="Hi, I'm Matthew!" className={styles.info_img} />
                 <div className={styles.info_text}>
                     <p>
                         I'm a self-taught developer in the Dallas-Fort Worth Metroplex who values communication, perseverence, and life long-learning.
--- src/assets/components/titlepage/Title.jsx
+++ src/assets/components/titlepage/Title.jsx
@@ -2,7 +2,7 @@ const Title = () => {
     return (<>
         <header>
             <h1 className="main-Title">
-                Hi I'm Matthew!
+                Hi, I'm Matthew!
             </h1>
             <h2 className="sub-Title">
                 Jr. Front End Web Developer
